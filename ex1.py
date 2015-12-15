c07848a4ee4012dfddd47082dbc4dcb9024fdc34
Steven Hemmy <chromatic.glissando@gmail.com>
2015-12-15 10:39:57 -0600

    Example 2 and 3

--- ex1.py
+++ ex1.py
@@ -3,6 +3,6 @@ print "Hello Again"
 print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
-print "I'd much rather you 'non'."
-print 'I "said" do not touch this.'
+print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
 print 'Here comes the sun.'
